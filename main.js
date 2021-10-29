ed0e2a9eebb782f2121c44d9fa0d6b4248dfce1b
Ramon Pereira <ramonpereira@MacBook-Pro-Ramon.local>
2021-10-28 22:39:19 -0300

    first upload

--- main.js
+++ main.js
@@ -2,9 +2,12 @@ const wrapper = document.querySelector('.wrapper'),
 inputPart = wrapper.querySelector('.input-part'),
 infoTxt = inputPart.querySelector('.info-txt'),
 inputField = inputPart.querySelector('input'),
-locationBtn = inputPart.querySelector('button');
+locationBtn = inputPart.querySelector('button'),
+weatherIcon = wrapper.querySelector('.weather-part img'),
+returnArrow = wrapper.querySelector('header i')
 
-let api
+
+let api;
 
 inputField.addEventListener('keyup', e => {
   if (e.key == 'Enter' && inputField.value != '') {
@@ -13,6 +16,12 @@ inputField.addEventListener('keyup', e => {
 })
 
 const apiKey = 'b161192fc8daeae1d0cd7f852973da9d';
+
+returnArrow.addEventListener('click', () => {
+  wrapper.classList.remove('active')
+  inputField.value = ''
+  inputField.focus()
+})
 
 locationBtn.addEventListener('click', () => {
   if (navigator.geolocation){
@@ -25,7 +34,7 @@ locationBtn.addEventListener('click', () => {
 function onSuccess(position) {
   // getting the latitude and longitude from the object coords
   const {latitude, longitude} = position.coords;
-  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}`
+  api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&appid=${apiKey}`
   fetchData()
 }
 
@@ -36,7 +45,7 @@ function onError(error) {
 }
 
 function requestApi(city) {
-  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+  api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
   fetchData()
 }
 
@@ -60,15 +69,29 @@ function weatherDetails(info) {
     const {description, id} = info.weather[0]
     const {feels_like, humidity, temp} = info.main
 
+    // Using specific images according to the weather codes provided in https://openweathermap.org/weather-conditions
+    if (id == 800) {
+      weatherIcon.src = './Assets/Weather Icons/clear.svg'
+    } else if (id >= 200 && id <= 232) {
+      weatherIcon.src = './Assets/Weather Icons/storm.svg'
+    } else if (id >= 600 && id <= 622) {
+      weatherIcon.src = './Assets/Weather Icons/snow.svg'
+    } else if (id >= 801 && id <= 804) {
+      weatherIcon.src = './Assets/Weather Icons/cloud.svg'
+    } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)) {
+      weatherIcon.src = './Assets/Weather Icons/rain.svg'
+    }
+
     // lets pass the values to the correct HTML element
-    wrapper.querySelector('temp, numb').innerText = temp
-    wrapper.querySelector('weather').innerText = description
+    wrapper.querySelector('.temp .numb').innerText = Math.floor(temp) 
+    wrapper.querySelector('.weather').innerText = description
     wrapper.querySelector('.location span').innerText = `${city}, ${country}`
-    wrapper.querySelector('.temp .numb-2').innerText = temp
-    wrapper.querySelector('temp, numb').innerText = temp
+    wrapper.querySelector('.temp .numb-2').innerText = Math.floor(feels_like)
+    wrapper.querySelector('.humidity span').innerText = `${humidity}%`
 
     infoTxt.classList.remove('pending', 'error')
     wrapper.classList.add('active')
     console.log(info)
   }
 }
+
